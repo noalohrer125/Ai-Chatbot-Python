1da348063fb61468d19a9d6b033c9a8d2bdfeea7
Magnus-90 <magnus.hansen@optimatik.ch>
2026-01-26 14:48:53 +0100

    Refactor chat functionality to maintain message history and improve output formatting

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
-from ollama import chat
-from ollama import ChatResponse
 import ollama
+import sys
 
 print("Willkommen zum AI Chatbot")
+print("-------------------------")
+print()
+print("Um denn Chat zu beenden: \033[36m/Exit\033[0m")
+print("Um einen Neuen Chat zu öffnen: \033[36m/New Chat\033[0")
+
+all_messages = []
 
 while True:
-  user_input = input("\n\033[31mUser: \033[0m")
-  response: ChatResponse = chat(model='llama3.1:8b', messages=[
-    {
-      'role': 'user',
-      'content': user_input,
-      "max_tokens": 200
-    },
-  ])
-  print("\033[32mOutput: \033[0m", end="", flush=True)
-
+  user_input = input("\n\033[1;31mUser: \033[0m")
+  all_messages.append({"role": "user", "content": user_input})
+  print("\033[1;32mOutput: \033[0m", end="", flush=True)
+  output_chatbot = ""
   stream = ollama.chat(
-        model='llama3.1:8b',
-        messages=[{'role': 'user', 'content': user_input}],
-        stream=True
-    )
+    model='llama3.1:8b',
+    messages=all_messages,
+      stream=True,
+      )
   for chunk in stream:
-    print(chunk.message.content, end="", flush=True)
+      print(chunk.message.content, end="", flush=True)
+      output_chatbot += chunk.message.content
+  all_messages.append({"role": "assistant", "content": output_chatbot})
