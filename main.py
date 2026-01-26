79c9df944aa70363b6eac6ab1e01a24d92979432
Magnus-90 <magnus.hansen@optimatik.ch>
2026-01-26 09:05:56 +0100

    Implement chat functionality and add requirements file for ollama

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from ollama import chat
 from ollama import ChatResponse
+import ollama
 
-response: ChatResponse = chat(model='llama3.1:8b', messages=[
-  {
-    'role': 'user',
-    'content': '',
-  },
-])
-# print(response['message']['content'])
-# or access fields directly from the response object
-print(response.message.content)
+print("Willkommen zum AI Chatbot")
+
+while True:
+  user_input = input("\n\033[31mUser: \033[0m")
+  response: ChatResponse = chat(model='llama3.1:8b', messages=[
+    {
+      'role': 'user',
+      'content': user_input,
+      "max_tokens": 200
+    },
+  ])
+  print("\033[32mOutput: \033[0m", end="", flush=True)
+
+  stream = ollama.chat(
+        model='llama3.1:8b',
+        messages=[{'role': 'user', 'content': user_input}],
+        stream=True
+    )
+  for chunk in stream:
+    print(chunk.message.content, end="", flush=True)
