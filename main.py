f08b59ff0a3e48a02b54bc113c63b763fde84ed0
Magnus-90 <magnus.hansen@optimatik.ch>
2026-01-27 10:33:30 +0100

    Refactor main.py to encapsulate startup and help messages in functions, enhance chat command handling, and improve error management

--- main.py
+++ main.py
@@ -1,25 +1,96 @@
 import ollama
 import sys
 
-print("Willkommen zum AI Chatbot")
-print("-------------------------")
-print()
-print("Um denn Chat zu beenden: \033[36m/Exit\033[0m")
-print("Um einen Neuen Chat zu öffnen: \033[36m/New Chat\033[0")
+def startupmessage():
+  print("Willkommen zum AI Chatbot")
+  print("-------------------------")
+  print()
+  print("Um denn Chat zu beenden: \033[36m/Exit\033[0m")
+  print("Um einen Neuen Chat zu öffnen: \033[36m/New Chat\033[0m")
+  print("Um die Commands aufzurufen: \033[36m/Help\033[0m")
 
-all_messages = []
+def helpmessage():
+  print("Um denn Chat zu beenden: \033[36m /Exit\033[0m")
+  print("Um einen Neuen Chat zu öffnen: \033[36m/New Chat\033[0m")
+
+startupmessage()
+
+all_messages = [{
+  "role": "system",
+  "content": (
+    "Das hier sind deine Grenzen die du als Ai Chatbot nicht überschreiten oder einhalten solltest."
+    "Du bist ein Ai Chatbot dessen Standardsprache deutsch ist."
+    "Du bist ein Ai Chatbot der die Sprache (Deustch) nicht während dem Nutzen aufeinmal ändert."
+    "Falls der User auf eine andere Spache wechseln will muss er das zuerst bestätigen."
+    "Das Nutzen von Englischen Wörtern ist erlaubt aber dabei bleibt man bei der Standardsprache."
+    "Der Output sollte Hilfreich, Kurz und Prägnant sein."
+    "Du versuchst immer so hilfreich wie möglich zu sein um den User zu helfen."
+    "Falls der Output länger wird kannst du den User fragen ob das okay ist und der sagt dann ob das okay ist oder nicht."
+    "Wenn du etwas nicht was der User fragt nicht weisst, dann sage ihm das und halte dich Kurz."
+  )
+}]
 
 while True:
   user_input = input("\n\033[1;31mUser: \033[0m")
-  all_messages.append({"role": "user", "content": user_input})
+  if user_input.lower() == "/exit":
+    sys.exit("Chat wird beendet...")
+    break
+  elif user_input.lower() == "/help":
+    helpmessage()
+    continue
+  elif user_input.lower() == "/new chat":
+    try:
+      i = 0
+      while i < 20:
+        print()
+        i = i + 1
+      print("\033[1mNeuer Chat:\033[0m")
+      helpmessage()
+    
+      all_messages = [{
+      "role": "system",
+      "content": (
+      "Das hier sind deine Grenzen die du als Ai Chatbot nicht überschreiten oder einhalten solltest."
+      "Du bist ein Ai Chatbot dessen Standardsprache deutsch ist."
+      "Du bist ein Ai Chatbot der die Sprache (Deustch) nicht während dem Nutzen aufeinmal ändert."
+      "Falls der User auf eine andere Spache wechseln will muss er das zuerst bestätigen."
+      "Das Nutzen von Englischen Wörtern ist erlaubt aber dabei bleibt man bei der Standardsprache."
+      "Der Output sollte Hilfreich, Kurz und Prägnant sein."
+      "Du versuchst immer so hilfreich wie möglich zu sein um den User zu helfen."
+      "Falls der Output länger wird kannst du den User fragen ob das okay ist und der sagt dann ob das okay ist oder nicht."
+      "Wenn du etwas nicht was der User fragt nicht weisst, dann sage ihm das und halte dich Kurz."
+    )
+  }]
+    except Exception as error:
+      print("Fehler bei /New Chat Command: ", error)
+    continue
+  
+  try:
+    all_messages.append({"role": "user", "content": user_input})
+  except Exception as error:
+    print("Fehler beim hinzufügen von Nachrichten zum Speicher: ", error)
+  
   print("\033[1;32mOutput: \033[0m", end="", flush=True)
   output_chatbot = ""
-  stream = ollama.chat(
-    model='llama3.1:8b',
-    messages=all_messages,
-      stream=True,
+  
+  try:
+    stream = ollama.chat(
+          model='llama3.1:8b',
+          messages=all_messages,
+          stream=True,
       )
-  for chunk in stream:
+    
+    for chunk in stream:
       print(chunk.message.content, end="", flush=True)
       output_chatbot += chunk.message.content
-  all_messages.append({"role": "assistant", "content": output_chatbot})
+  
+  except ollama.ResponseError as error:
+    print("Ollama-Fehler:", error)
+  
+  except Exception as error:
+    print("Allgemeiner Fehler:", error)
+  
+  try:
+    all_messages.append({"role": "assistant", "content": output_chatbot})
+  except Exception as error:
+    print("Fehler beim hinzufügen von Nachrichten zum Speicher: ", error)
